4a61d898c9356a709e800d159c58527f2d270306
Bruno Brant <1890900+bruno-brant@users.noreply.github.com>
2021-04-05 14:42:45 -0300

    Add project files.

---
+++ NewtonsoftRecordIssues/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace NewtonsoftRecordIssues
+{
+	class Program
+	{
+		static void Main()
+		{
+			var r = new MyRecord(Gender.Male);
+
+			Console.WriteLine(JsonConvert.SerializeObject(r));
+
+			var c = new MyClass { Gender = Gender.Male };
+
+			Console.WriteLine(JsonConvert.SerializeObject(c));
+		}
+	}
+
+	public enum Gender
+	{
+		Male,
+		Female
+	}
+
+
+	public record MyRecord([JsonConverter(typeof(StringEnumConverter))] Gender Gender);
+
+	public class MyClass {[JsonConverter(typeof(StringEnumConverter))] public Gender Gender { get; init; } }
+}
